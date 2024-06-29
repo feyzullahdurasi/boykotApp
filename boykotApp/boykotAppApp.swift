6dd2eed27757b8c6c8252030689c3c8fa7788120
Feyzullah Durası <durasifeyzullah672@gmail.com>
2024-06-29 12:02:02 +0300

    Initial Commit

---
+++ boykotApp/boykotAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  boykotAppApp.swift
+//  boykotApp
+//
+//  Created by Feyzullah Durası on 29.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct boykotAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
